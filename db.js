1157c93803942ec12b3e784eb3cf06e5636a8d9d
cbatuic <cbalaman@uic.edu.ph>
2021-05-26 16:11:38 +0800

    second commit

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "",
-    password: "",
-    host: "",
+    user: "saooamtqctzgxw",
+    password: "d79ab53c0b41691f9695a209e0565a9aa1531bd21051e129628108f383de490f",
+    host: "ec2-18-214-140-149.compute-1.amazonaws.com",
     port: 5432,
-    database: ""
+    database: "dfu58nrf1uaoij",
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -11,6 +11,16 @@ app.get("/",(req,res)=>{
     res.send("DVD Rental Server is up!")
 });
 
+app.get("/test", async (req,res)=>{
+    try {
+        const sql = "select * from film";
+        const data = await pool.query(sql);
+        res.json(data.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`DVD Rental Server is running on port ${ PORT }`);
